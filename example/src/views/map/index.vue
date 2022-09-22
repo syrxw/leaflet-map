55b0472f344aca1da9117ed40fe434bfbefa6add
Wenyu Wang <wangwenyu9394@gmail.com>
2022-09-22 13:49:25 +0800

    update: 增加geojson叠加效果

--- example/src/views/map/index.vue
+++ example/src/views/map/index.vue
@@ -7,16 +7,28 @@ import { ref, onMounted, reactive } from "vue";
 
 import { map as drawMap } from "@root/lib/leaflet-map-es";
 import mapConfig from "@/config/map";
+import lf from "@/mock/422827.json";
 
 let gisMap;
 function mapInit() {
+  mapConfig.map.zoom = 11;
+  mapConfig.map.center = [29.495755, 109.4032717];
   gisMap = drawMap.createMap(mapConfig);
-  mapConfig.map.type = "Mapbox.Satellite.Map";
   let tileLayer = drawMap.addPresetTileLayer(mapConfig);
 
   // new window.L.tileLayer(
   //   "https://api.mapbox.com/styles/v1/osdwenyu/cl6oh94u9002w15mg25o0afsj/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib3Nkd2VueXUiLCJhIjoiY2w2b2g2bnllMDFxMzNjcGlwcXNxazRrNyJ9.QBEkrrQwNHRvBy81N5STAw"
   // ).addTo(gisMap);
+
+  let wfsLayer = window.L.geoJson(lf, {
+    style: {
+      fill: true,
+      fillOpacity: 0.1,
+      weight: 2,
+      dashArray: "8, 8",
+    },
+  });
+  wfsLayer.addTo(gisMap);
 }
 
 onMounted(async () => {
@@ -29,5 +41,10 @@ onMounted(async () => {
   width: 100vw;
   height: 100vh;
   z-index: 1;
+}
+
+.leaflet-zoom-animated img {
+  filter: brightness(0.88) contrast(1.22) grayscale(0) hue-rotate(360deg) opacity(1) saturate(1.1)
+    sepia(0.54) invert(0.9);
 }
 </style>
